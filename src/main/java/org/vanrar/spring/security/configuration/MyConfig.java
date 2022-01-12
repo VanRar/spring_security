b93e6dff23ab84408aff3827e0e686385e4fc6bc
VanRar <vitalikf7@gmail.com>
2022-01-12 21:54:37 +0300

    passwords are transferred to the database

--- src/main/java/org/vanrar/spring/security/configuration/MyConfig.java
+++ src/main/java/org/vanrar/spring/security/configuration/MyConfig.java
@@ -1,11 +1,16 @@
 package org.vanrar.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "org.vanrar.spring.security")
@@ -20,4 +25,19 @@ public class MyConfig {
 
         return internalResourceViewResolver;
     }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
+            dataSource.setUser("root");
+            dataSource.setPassword("springcourse");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
 }
